94fc1cc9921a5760788f9eb1d2faeaf93c8e34b9
Joshua Dietrich <jbdietrich2@gmail.com>
2024-03-13 05:45:48 -0600

    working on pricing page

---
+++ src/assets/Information/additonalFees.js
@@ -0,0 +1,34 @@
+const AdditionalFees = [
+  {
+    id: 1,
+    name: "Belt test Fees.",
+    description:
+      "Belt Test Fees. From Yellow Stripe through Purple Belt, the Belt Test Fee is $45. From Red Belt through Gray Belt, it’s $60. For Black Belt, it’s $500. ",
+  },
+  {
+    id: 2,
+    name: "Starting Fees",
+    description:
+      "I do require that you purchase a gi (karate uniform) and sparring gear right at the start. This can cost anywhere from $150-$300+ depending on how nice of gear you want to purchase.",
+  },
+  {
+    id: 3,
+    name: "POTENTIAL Tournoment Fees",
+    description:
+      "Anywhere from $20-$150 per tournament. I don’t anticipate we’ll be doing any MAJOR tournaments anytime soon, but I believe competition is extremely good for improvement, so we’ll try our best to arrange for something, even if it’s very small and only once per year.",
+  },
+  {
+    id: 4,
+    name: "Additional Equipment Fees",
+    description:
+      "Throughout your time, you may wish to purchase additional karate gis, nicer sparring gear, or additional weapons. At some point we may sell optional dojo merchandise.",
+  },
+  {
+    id: 5,
+    name: "Black Belt Club Fee",
+    description:
+      "Once you join the Black Belt Club (available at Green Belt and beyond), you will pay a one-time fee of $50.00. Joining the Black Belt club is optional in the intermediate ranks, but is required for Red Belt and above.",
+  },
+];
+
+export default AdditionalFees;
--- src/pages/pricing/pricingpage.tsx
+++ src/pages/pricing/pricingpage.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import Banner from "../../components/banner/banner";
 
+import AdditionalFees from "../../assets/Information/additonalFees";
+
+import "./pricing.styles.scss";
+
 const PricingPage: React.FC = () => {
   return (
     <div>
@@ -10,7 +14,42 @@ const PricingPage: React.FC = () => {
         subtitle=""
         subtitle2={["Affordable", "transparent"]}
       />
-      <div>PricingPage</div>
+      <div className="sectionContainer">
+        <div className="sectionHeading">Pricing Mission</div>
+        <div className="sectionContent">
+          {" "}
+          We have two missions when it comes to our pricing. First, we want to
+          lower the barrier of entry for people to begin their martial arts
+          journey by having the most affordable prices in town WITHOUT
+          compromising on value and quality . Second, we seek to be transparent
+          with our pricing. We hate it (and suspect you do too) when businesses
+          try to hide how expensive something is going to be. As is the case
+          with many sports, tuition is not the only cost you can expect, and
+          we’ve done our honest best to list any other things you can expect to
+          pay for here.
+        </div>
+      </div>
+      <div className="sectionContainer">
+        <div className="sectionHeading">Tuition</div>
+        <div className="sectionContent">
+          $50.00/month. No annual contract. Contact us to set up your TWO FREE
+          TRIAL CLASSES!
+        </div>
+      </div>
+      <div className="sectionContainer">
+        <div className="sectionHeading">Additional Fee's</div>
+        <div className="sectionContent">
+          {AdditionalFees.map((item) => {
+            return (
+              <div className="feesContainer">
+                <div className="feeNumber">{item.id} - </div>
+                <div className="feeName"> {item.name}:</div>
+                <div className="feeDescription">{item.description}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
