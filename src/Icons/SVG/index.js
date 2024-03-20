43436bdf0152dbbf573a54c12cc2be2f6955f9f6
Joshua Dietrich <jbdietrich2@gmail.com>
2024-03-19 23:36:03 -0600

    added kick animation

---
+++ src/Icons/SVG/index.js
@@ -0,0 +1,5 @@
+export { default as KaratePose1Test } from "./KaratePose1Test";
+export { default as Kick } from "./Kick";
+export { default as Pose1 } from "./Pose1";
+export { default as Pose2 } from "./Pose2";
+export { default as Pose3 } from "./Pose3";
--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import "./dropdown.styles.scss";
 
+import SvgPose1 from "../../Icons/SVG/Pose1.js";
+
 interface DropdownProps {
   key: number;
   id: number;
@@ -12,23 +14,35 @@ interface DropdownProps {
 const Dropdown: React.FC<DropdownProps> = (props) => {
   const { q, a, id } = props;
   const [activeIndex, setActiveIndex]: any[] = useState([]);
+  const [imgIndex, setImgIndex]: any = useState("1");
 
   const onTitleClick = (id: number) => {
     if (activeIndex.includes(id)) {
       const tempArr = activeIndex.filter((item: number) => item !== id);
       setActiveIndex(tempArr);
-      console.log("includes");
+      setImgIndex("2");
+      setTimeout(() => {
+        setImgIndex("1");
+      }, 500);
     } else {
       setActiveIndex([...activeIndex, id]);
-      console.log("does not include");
+      setImgIndex("2");
+      setTimeout(() => {
+        setImgIndex("3");
+      }, 400);
     }
   };
 
   return (
     <div key={id} onClick={() => onTitleClick(id)} className="dropdown">
       <div className={activeIndex.includes(id) ? `selected` : "question"}>
-        <div>{activeIndex.includes(id) ? "-" : "+"}</div>
-        {q}
+        <img
+          className="kickImg"
+          src={require(`../../assets/Kick/pose${imgIndex}.png`)}
+          alt="karate kick"
+        />
+        {/* <div>{activeIndex.includes(id) ? "-" : "+"}</div> */}
+        <div className="questionText">{q}</div>
       </div>
       <div className={activeIndex.includes(id) ? `answer-active` : "answer"}>
         {a.map((item) => {
